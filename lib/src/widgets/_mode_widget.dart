bf10d6fae84b7a9f4d46e38e31a32a45dafad69b
Felix Malinowski <fmalinowski@enloc.de>
2022-10-13 15:17:24 +0200

    feat: hide text option

--- lib/src/widgets/_mode_widget.dart
+++ lib/src/widgets/_mode_widget.dart
@@ -68,10 +68,10 @@ List<ModeData> paintModes(TextDelegate textDelegate) => [
           icon: Icons.power_input,
           mode: PaintMode.dashLine,
           label: textDelegate.dashLine),
-      ModeData(
-          icon: Icons.text_format,
-          mode: PaintMode.text,
-          label: textDelegate.text),
+      // ModeData(
+      //     icon: Icons.text_format,
+      //     mode: PaintMode.text,
+      //     label: textDelegate.text),
     ];
 
 @immutable
