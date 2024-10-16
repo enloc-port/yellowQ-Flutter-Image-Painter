0bd99fb82d945681a9994f0c77f62032117c2c3e
Felix Malinowski <fmalinowski@enloc.de>
2024-10-16 22:57:44 +0200

    feat: add optional parameter "customPaintModes" to paintModes() method and return ModeData list accordingly

--- lib/src/widgets/_mode_widget.dart
+++ lib/src/widgets/_mode_widget.dart
@@ -51,40 +51,53 @@ class SelectionItems extends StatelessWidget {
   }
 }
 
-List<ModeData> paintModes(TextDelegate textDelegate) => [
-      ModeData(
-          icon: Icons.zoom_out_map,
-          mode: PaintMode.none,
-          label: textDelegate.noneZoom),
-      ModeData(
-          icon: Icons.horizontal_rule,
-          mode: PaintMode.line,
-          label: textDelegate.line),
-      ModeData(
-          icon: Icons.crop_free,
-          mode: PaintMode.rect,
-          label: textDelegate.rectangle),
-      ModeData(
-          icon: Icons.edit,
-          mode: PaintMode.freeStyle,
-          label: textDelegate.drawing),
-      ModeData(
-          icon: Icons.lens_outlined,
-          mode: PaintMode.circle,
-          label: textDelegate.circle),
-      ModeData(
-          icon: Icons.arrow_right_alt_outlined,
-          mode: PaintMode.arrow,
-          label: textDelegate.arrow),
-      ModeData(
-          icon: Icons.power_input,
-          mode: PaintMode.dashLine,
-          label: textDelegate.dashLine),
-      ModeData(
-          icon: Icons.text_format,
-          mode: PaintMode.text,
-          label: textDelegate.text),
-    ];
+List<ModeData> paintModes(TextDelegate textDelegate, [List<PaintMode>? customPaintModes]) {
+  final Map<PaintMode, ModeData> allPaintModes = {
+    PaintMode.none: ModeData(
+        icon: Icons.zoom_out_map,
+        mode: PaintMode.none,
+        label: textDelegate.noneZoom),
+    PaintMode.line: ModeData(
+        icon: Icons.horizontal_rule,
+        mode: PaintMode.line,
+        label: textDelegate.line),
+    PaintMode.rect: ModeData(
+        icon: Icons.crop_free,
+        mode: PaintMode.rect,
+        label: textDelegate.rectangle),
+    PaintMode.freeStyle: ModeData(
+        icon: Icons.edit,
+        mode: PaintMode.freeStyle,
+        label: textDelegate.drawing),
+    PaintMode.circle: ModeData(
+        icon: Icons.lens_outlined,
+        mode: PaintMode.circle,
+        label: textDelegate.circle),
+    PaintMode.arrow: ModeData(
+        icon: Icons.arrow_right_alt_outlined,
+        mode: PaintMode.arrow,
+        label: textDelegate.arrow),
+    PaintMode.dashLine: ModeData(
+        icon: Icons.power_input,
+        mode: PaintMode.dashLine,
+        label: textDelegate.dashLine),
+    PaintMode.text: ModeData(
+        icon: Icons.text_format,
+        mode: PaintMode.text,
+        label: textDelegate.text),
+  };
+  
+  final List<ModeData> customModeData = <ModeData>[];
+  
+  if (customPaintModes != null) {
+    for (final PaintMode mode in customPaintModes) {
+      customModeData.add(allPaintModes[mode]!);
+    }
+    return customModeData;
+  } else {
+    return allPaintModes.values.toList();
+  }
+}
 
 @immutable
 class ModeData {
